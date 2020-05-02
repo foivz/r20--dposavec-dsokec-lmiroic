97ed1156c818a06a4573f1f11596576e6822bdd4
Luka <lmiroic@foi.hr>
2020-05-02 12:41:03 +0200

    Podignuta arhitektura programskog proizvoda. Napravljena troslojna arhitektura koja se sastoji od prezentacijskog sloja, sloja poslovne logike i sloja pristupa podacima, napravljena na način da prezentacijski sloj može vidjeti sloj poslovne logike i sloj pristupa podacima, a sloj poslovne logike može vidjeti sloj pristupa podacima.

---
+++ Software/CarDealershipService/Sloj pristupa podacima/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sloj_pristupa_podacima
+{
+    public class Class1
+    {
+    }
+}
