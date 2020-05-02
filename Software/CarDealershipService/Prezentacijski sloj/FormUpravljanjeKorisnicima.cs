f52beaa87ad1f722fcf80ff805a47319d335f22f
Luka <lmiroic@foi.hr>
2020-05-02 13:26:13 +0200

    Kreirana forma za upravljanje korisnicima.

---
+++ Software/CarDealershipService/Prezentacijski sloj/FormUpravljanjeKorisnicima.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prezentacijski_sloj
+{
+    public partial class FormUpravljanjeKorisnicima : Form
+    {
+        public FormUpravljanjeKorisnicima()
+        {
+            InitializeComponent();
+        }
+    }
+}
