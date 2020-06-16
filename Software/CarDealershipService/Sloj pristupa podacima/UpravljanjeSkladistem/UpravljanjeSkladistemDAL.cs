2ce63cf7ce1aa616c0fdba1fb2f17fa64db02060
Luka <lmiroic@foi.hr>
2020-06-16 16:41:52 +0200

    Kreirane klase za upravljanje skladištem u sloju poslovne logike i sloju pristupa podacima.

---
+++ Software/CarDealershipService/Sloj pristupa podacima/UpravljanjeSkladistem/UpravljanjeSkladistemDAL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sloj_pristupa_podacima.UpravljanjeSkladistem
+{
+    public class UpravljanjeSkladistemDAL
+    {
+        public static List<Artikl> VratiSveArtikle()
+        {
+            List<Artikl> SviArtikli = null;
+            using (var db = new CarDealershipandServiceEntities())
+            {
+                SviArtikli = db.Artikls.ToList();
+            }
+            return SviArtikli;
+        }
+    }
+}
