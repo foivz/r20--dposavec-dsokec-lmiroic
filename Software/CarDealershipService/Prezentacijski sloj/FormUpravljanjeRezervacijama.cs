2dbc9f2f7bc697f3f16319db7090b678e79e3754
Dominik <dposavec@foi.hr>
2020-06-16 20:27:30 +0200

    Izrađene klase Upravljanje rezervacijama u sloju pristupa podataka i sloju poslovne logike. Izrađen dizajn forme Upravljane rezervacijama te implementiran sloj na bazu podataka.

--- Software/CarDealershipService/Prezentacijski sloj/FormUpravljanjeRezervacijama.cs
+++ Software/CarDealershipService/Prezentacijski sloj/FormUpravljanjeRezervacijama.cs
@@ -27,11 +27,67 @@ namespace Prezentacijski_sloj
         public FormUpravljanjeRezervacijama()
         {
             InitializeComponent();
+            panelUpravRezervacijamaHeader.BackColor = Color.FromArgb(45, 45, 48);
+            panelUpravRezervacijamaFooter.BackColor = Color.FromArgb(45, 45, 48);
         }
 
         private void FormUpravljanjeRezervacijama_FormClosed(object sender, FormClosedEventArgs e)
         {
             _instance = null;
+            
+        }
+
+        private void FormUpravljanjeRezervacijama_Load(object sender, EventArgs e)
+        {
+            dgvUpravRezervacijamaSveRezervacije.DataSource = null;
+            dgvUpravRezervacijamaSveRezervacije.DataSource = Sloj_pristupa_podacima.UpravljanjeRezervacijama.UpravljanjeRezervacijamaDAL.DohvatiSveRezervacije();
+            dgvUpravRezervacijamaSveRezervacije.Columns[7].Visible = false;
+            dgvUpravRezervacijamaSveRezervacije.Columns[8].Visible = false;
+            dgvUpravRezervacijamaSveRezervacije.Columns[9].Visible = false;
+            dgvUpravRezervacijamaSveRezervacije.Columns[10].Visible = false;
+        }
+        private Button currentButton;
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    Color color = Color.FromArgb(30, 30, 30);
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.White;
+                    currentButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panelUpravRezervacijamaFooter.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(45, 45, 48);
+                    previousBtn.ForeColor = Color.LightGray;
+                    previousBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                }
+            }
+        }
+
+        private void uiActionAzuriranjRervacije_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void uiActionObrisiRezervacije_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void uiActionKreirajRezervaciju_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
         }
     }
 }
---
+++ Software/CarDealershipService/Sloj pristupa podacima/UpravljanjeRezervacijama/UpravljanjeRezervacijamaDAL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sloj_pristupa_podacima.UpravljanjeRezervacijama
+{
+    public class UpravljanjeRezervacijamaDAL
+    {
+        public static List<Dokument> DohvatiSveRezervacije()
+        {
+            List<Dokument> SveRezervacije = null;
+            using (var db = new CarDealershipandServiceEntities())
+            {
+                var DohvacenaRezervacija = from r in db.Dokuments
+                                           where r.tip_dokumenta == 4
+                                           select r;
+                SveRezervacije = DohvacenaRezervacija.ToList();
+            }
+            return SveRezervacije;
+        }
+
+    }
+}
