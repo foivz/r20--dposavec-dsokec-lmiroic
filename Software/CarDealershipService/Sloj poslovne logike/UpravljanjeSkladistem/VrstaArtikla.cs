c5e951f943e85cfa8e096164f9f63a204cb01f17
Luka <lmiroic@foi.hr>
2020-06-17 21:56:49 +0200

    Kreirana klasa VrstaArtikla.

---
+++ Software/CarDealershipService/Sloj poslovne logike/UpravljanjeSkladistem/VrstaArtikla.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sloj_poslovne_logike.UpravljanjeSkladistem
+{
+    class VrstaArtikla:Sloj_pristupa_podacima.Vrste_artikla
+    {
+        public override string ToString()
+        {
+            return naziv;
+        }
+    }
+}
