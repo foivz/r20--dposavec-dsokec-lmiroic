3397f7579c968d26ce2db312449ab4452b509d27
Luka <lmiroic@foi.hr>
2020-06-17 21:57:21 +0200

    Kreirana klasa ParserArtikla.

---
+++ Software/CarDealershipService/Sloj poslovne logike/UpravljanjeSkladistem/ParserVrsteArtikla.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sloj_poslovne_logike.UpravljanjeSkladistem
+{
+    class ParserVrsteArtikla
+    {
+        public static List<VrstaArtikla> ParsirajVrsteArtikla()
+        {
+            List<VrstaArtikla> vrsteArtikla = new List<VrstaArtikla>();
+            foreach (var item in Sloj_pristupa_podacima.UpravljanjeSkladistem.UpravljanjeSkladistemDAL.VratiSveVrsteArtikla())
+            {
+                VrstaArtikla v = new VrstaArtikla();
+                v.id_vrste_artikla = item.id_vrste_artikla;
+                v.naziv = item.naziv;
+                v.Artikls = item.Artikls;
+                vrsteArtikla.Add(v);
+            }
+            return vrsteArtikla;
+        }
+    }
+}
