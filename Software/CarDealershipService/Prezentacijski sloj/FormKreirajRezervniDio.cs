1de621ee11a90724798a424e8a516bf0ffee7df5
Luka <lmiroic@foi.hr>
2020-06-19 20:06:25 +0200

    Implementirana provjera korisničkog unosa u formi KreirajRezervniDio.

--- Software/CarDealershipService/Prezentacijski sloj/FormKreirajRezervniDio.cs
+++ Software/CarDealershipService/Prezentacijski sloj/FormKreirajRezervniDio.cs
@@ -36,36 +36,58 @@ namespace Prezentacijski_sloj
         private void FormKreirajRezervniDio_FormClosed(object sender, FormClosedEventArgs e)
         {
             _instance = null;
+            proslijedeniArtikl = null;
         }
 
         private void uiActionSpremi_Click(object sender, EventArgs e)
         {
             Sloj_pristupa_podacima.Artikl artikl = new Sloj_pristupa_podacima.Artikl();
+            try
             {
-                artikl.godina_proizvodnje = int.Parse(uiInputGodinaProizvodnjeDijela.Text);               
-                artikl.opis_artikla = uiInputOpisRezervnogDijela.Text;                
+                artikl.godina_proizvodnje = int.Parse(uiInputGodinaProizvodnjeDijela.Text);
+                artikl.opis_artikla = uiInputOpisRezervnogDijela.Text;
                 artikl.naziv_artikla = uiInputNazivRezervnogDijela.Text;
                 artikl.cijena_artikla = double.Parse(uiInputCijenaRezervnogDijela.Text);
                 artikl.vrsta_artikla = 1;
+                if (Sloj_poslovne_logike.UpravljanjeSkladistem.UpravljanjeSkladistemBLL.ProvjeraUnosaRezervnogDijela(artikl) == true)
+                {
+                    Sloj_pristupa_podacima.UpravljanjeSkladistem.UpravljanjeSkladistemDAL.KreiranjeArtikla(artikl);
+                    FormUpravljanjeSkladistem.OsvjeziPopisArtikala();
+                }
+                else
+                {
+                    MessageBox.Show("Niste unijeli odgovarajuće parametre! Za pomoć pritisnite F1.");
+                }
             }
-            Sloj_pristupa_podacima.UpravljanjeSkladistem.UpravljanjeSkladistemDAL.KreiranjeArtikla(artikl);
-            FormUpravljanjeSkladistem.OsvjeziPopisArtikala();
+            catch (Exception)
+            {
+
+                MessageBox.Show("Morate unijeti sve parametre");
+            }
         }
 
         private void FormKreirajRezervniDio_Load(object sender, EventArgs e)
         {
-            if (proslijedeniArtikl!=null)
+            if (proslijedeniArtikl != null)
             {
                 uiInputCijenaRezervnogDijela.Text = proslijedeniArtikl.cijena_artikla.ToString();
                 uiInputGodinaProizvodnjeDijela.Text = proslijedeniArtikl.godina_proizvodnje.ToString();
                 uiInputNazivRezervnogDijela.Text = proslijedeniArtikl.naziv_artikla;
                 uiInputOpisRezervnogDijela.Text = proslijedeniArtikl.opis_artikla;
+                uiActionSpremi.Enabled = false;
             }
+            else
+            {
+                uiActionAzuriraj.Enabled = false;
+                uiActionAzuriraj.Hide();
+            }
+                
         }
 
         private void uiActionAzuriraj_Click(object sender, EventArgs e)
         {
             Sloj_pristupa_podacima.Artikl artikl = new Sloj_pristupa_podacima.Artikl();
+            try
             {
                 artikl.id_artikl = proslijedeniArtikl.id_artikl;
                 artikl.godina_proizvodnje = int.Parse(uiInputGodinaProizvodnjeDijela.Text);
@@ -73,10 +95,21 @@ namespace Prezentacijski_sloj
                 artikl.naziv_artikla = uiInputNazivRezervnogDijela.Text;
                 artikl.cijena_artikla = double.Parse(uiInputCijenaRezervnogDijela.Text);
                 artikl.vrsta_artikla = 1;
+                if (Sloj_poslovne_logike.UpravljanjeSkladistem.UpravljanjeSkladistemBLL.ProvjeraUnosaRezervnogDijela(artikl) == true)
+                {
+                    Sloj_pristupa_podacima.UpravljanjeSkladistem.UpravljanjeSkladistemDAL.AzurirajArtikl(artikl);
+                    FormUpravljanjeSkladistem.OsvjeziPopisArtikala();
+                }
+                else
+                {
+                    MessageBox.Show("Niste unijeli odgovarajuće parametre! Za pomoć pritisnite F1.");
+                }
             }
-            Sloj_pristupa_podacima.UpravljanjeSkladistem.UpravljanjeSkladistemDAL.AzurirajArtikl(artikl);
-            FormUpravljanjeSkladistem.OsvjeziPopisArtikala();
-            proslijedeniArtikl = null;
+            catch (Exception)
+            {
+
+                MessageBox.Show("Morate unijeti sve parametre");
+            }
         }
     }
 }
