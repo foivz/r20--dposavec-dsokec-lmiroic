7651ef27e796f85a193fc13f2fdaf7f0852c4750
Luka <lmiroic@foi.hr>
2020-06-19 21:51:46 +0200

    Implementirana provjera korisničkog unosa u formi KreirajKorisnika.

--- Software/CarDealershipService/Prezentacijski sloj/FormKreirajKorisnika.cs
+++ Software/CarDealershipService/Prezentacijski sloj/FormKreirajKorisnika.cs
@@ -36,18 +36,32 @@ namespace Prezentacijski_sloj
         private void uiActionKreiraj_Click(object sender, EventArgs e)
         {
             Sloj_pristupa_podacima.Korisnik korisnik = new Sloj_pristupa_podacima.Korisnik();
-            korisnik.ime_korisnika = uiInputImeKorisnika.Text;
-            korisnik.prezime_korisnika = uiInputPrezimeKorisnika.Text;
-            korisnik.adresa = uiInputAdresaKorisnika.Text;
-            korisnik.email = uiInputMailKorisnika.Text;
-            korisnik.IBAN = uiInputIBANKorisnika.Text;
-            korisnik.korisnicko_ime = uiInputKorisnickoIme.Text;
-            korisnik.lozinka = Sloj_pristupa_podacima.UpravljanjeKorisnicima.UpravljanjeKorisnicima.HashirajLozinku(uiInputLozinka.Text);
-            korisnik.tip_korisnika = (uiInputTipKorisnika.SelectedItem as Sloj_pristupa_podacima.Tip_korisnika).id_tip_korisnika;
-            korisnik.poslovnica = (uiInputKorisnikovaPoslovnica.SelectedItem as Sloj_pristupa_podacima.Poslovnica).id_poslovnica;
+            try
+            {
+                korisnik.ime_korisnika = uiInputImeKorisnika.Text;
+                korisnik.prezime_korisnika = uiInputPrezimeKorisnika.Text;
+                korisnik.adresa = uiInputAdresaKorisnika.Text;
+                korisnik.email = uiInputMailKorisnika.Text;
+                korisnik.IBAN = uiInputIBANKorisnika.Text;
+                korisnik.korisnicko_ime = uiInputKorisnickoIme.Text;
+                korisnik.lozinka = Sloj_pristupa_podacima.UpravljanjeKorisnicima.UpravljanjeKorisnicima.HashirajLozinku(uiInputLozinka.Text);
+                korisnik.tip_korisnika = (uiInputTipKorisnika.SelectedItem as Sloj_pristupa_podacima.Tip_korisnika).id_tip_korisnika;
+                korisnik.poslovnica = (uiInputKorisnikovaPoslovnica.SelectedItem as Sloj_pristupa_podacima.Poslovnica).id_poslovnica;
+                if (Sloj_poslovne_logike.UpravljanjeKorisnicima.UpravljanjeKorisnicimaBLL.ProvjeriUnosKorisnika(korisnik)==true)
+                {
+                    Sloj_pristupa_podacima.UpravljanjeKorisnicima.UpravljanjeKorisnicima.KreiranjeKorisnika(korisnik);
+                    FormUpravljanjeKorisnicima.OsvjeziPopisKorisnika();
+                }
+                else
+                {
+                    MessageBox.Show("Niste unijeli odgovarajuće parametre! Za pomoć pritisnite F1.");
+                }
+            }
+            catch (Exception)
+            {
 
-            Sloj_pristupa_podacima.UpravljanjeKorisnicima.UpravljanjeKorisnicima.KreiranjeKorisnika(korisnik);
-            FormUpravljanjeKorisnicima.OsvjeziPopisKorisnika();
+                MessageBox.Show("Morate unijeti sve parametre!");
+            }
         }
 
         private void FormKreirajKorisnika_Load(object sender, EventArgs e)
@@ -73,6 +87,13 @@ namespace Prezentacijski_sloj
                 {
                     uiInputKorisnikovaPoslovnica.SelectedIndex = listaPoslovnica.IndexOf(listaPoslovnica.Find(x => x.id_poslovnica == ProsljedeniKorisnik.poslovnica));
                 }
+                uiActionKreiraj.Enabled = false;
+                uiActionKreiraj.Hide();
+            }
+            else
+            {
+                uiActionAzuriranje.Enabled = false;
+                uiActionAzuriranje.Hide();
             }
 
         }
@@ -80,29 +101,44 @@ namespace Prezentacijski_sloj
         private void uiActionAzuriranje_Click(object sender, EventArgs e)
         {
             Sloj_pristupa_podacima.Korisnik korisnik = new Sloj_pristupa_podacima.Korisnik();
-            korisnik.id_korisnik = ProsljedeniKorisnik.id_korisnik;
-            korisnik.ime_korisnika = uiInputImeKorisnika.Text;
-            korisnik.prezime_korisnika = uiInputPrezimeKorisnika.Text;
-            korisnik.adresa = uiInputAdresaKorisnika.Text;
-            korisnik.email = uiInputMailKorisnika.Text;
-            korisnik.IBAN = uiInputIBANKorisnika.Text;
-            korisnik.korisnicko_ime = uiInputKorisnickoIme.Text;
-            korisnik.lozinka = Sloj_pristupa_podacima.UpravljanjeKorisnicima.UpravljanjeKorisnicima.HashirajLozinku(uiInputLozinka.Text);
-            korisnik.tip_korisnika = (uiInputTipKorisnika.SelectedItem as Sloj_pristupa_podacima.Tip_korisnika).id_tip_korisnika;
-            if (ProsljedeniKorisnik.poslovnica != null)
+            try
             {
-                korisnik.poslovnica = (uiInputKorisnikovaPoslovnica.SelectedItem as Sloj_pristupa_podacima.Poslovnica).id_poslovnica;
+                korisnik.id_korisnik = ProsljedeniKorisnik.id_korisnik;
+                korisnik.ime_korisnika = uiInputImeKorisnika.Text;
+                korisnik.prezime_korisnika = uiInputPrezimeKorisnika.Text;
+                korisnik.adresa = uiInputAdresaKorisnika.Text;
+                korisnik.email = uiInputMailKorisnika.Text;
+                korisnik.IBAN = uiInputIBANKorisnika.Text;
+                korisnik.korisnicko_ime = uiInputKorisnickoIme.Text;
+                korisnik.lozinka = Sloj_pristupa_podacima.UpravljanjeKorisnicima.UpravljanjeKorisnicima.HashirajLozinku(uiInputLozinka.Text);
+                korisnik.tip_korisnika = (uiInputTipKorisnika.SelectedItem as Sloj_pristupa_podacima.Tip_korisnika).id_tip_korisnika;
+                if (ProsljedeniKorisnik.poslovnica != null)
+                {
+                    korisnik.poslovnica = (uiInputKorisnikovaPoslovnica.SelectedItem as Sloj_pristupa_podacima.Poslovnica).id_poslovnica;
+                }
+                else
+                    korisnik.poslovnica = null;
+                if (Sloj_poslovne_logike.UpravljanjeKorisnicima.UpravljanjeKorisnicimaBLL.ProvjeriUnosKorisnika(korisnik)==true)
+                {
+                    Sloj_pristupa_podacima.UpravljanjeKorisnicima.UpravljanjeKorisnicima.AzurirajKorisnika(korisnik);
+                    FormUpravljanjeKorisnicima.OsvjeziPopisKorisnika();
+                }
+                else
+                {
+                    MessageBox.Show("Niste unijeli odgovarajuće parametre! Za pomoć pritisnite F1.");
+                }
             }
-            else
-                korisnik.poslovnica = null;
-            Sloj_pristupa_podacima.UpravljanjeKorisnicima.UpravljanjeKorisnicima.AzurirajKorisnika(korisnik);
-            FormUpravljanjeKorisnicima.OsvjeziPopisKorisnika();
-            ProsljedeniKorisnik = null;
+            catch (Exception)
+            {
+
+                MessageBox.Show("Morate unijeti sve parametre!");
+            }
         }
 
         private void FormKreirajKorisnika_FormClosed(object sender, FormClosedEventArgs e)
         {
             _Instance = null;
+            ProsljedeniKorisnik = null;
         }
     }
 }
