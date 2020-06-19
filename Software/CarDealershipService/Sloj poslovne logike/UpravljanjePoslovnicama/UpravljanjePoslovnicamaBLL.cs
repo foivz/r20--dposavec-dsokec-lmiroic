e86eb6ba66baa9d2ff81fb10d7bfe9c4fa04586f
Luka <lmiroic@foi.hr>
2020-06-19 20:56:56 +0200

    Kreirana klasa UpravljanjePoslovnicamaBLL koja sadrži funkciju ProvjeraUnosaPoslovnice.

---
+++ Software/CarDealershipService/Sloj poslovne logike/UpravljanjePoslovnicama/UpravljanjePoslovnicamaBLL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sloj_poslovne_logike.UpravljanjePoslovnicama
+{
+    public class UpravljanjePoslovnicamaBLL
+    {
+        public static bool ProvjeraUnosaPoslovnice(Sloj_pristupa_podacima.Poslovnica poslovnica)
+        {
+            if (poslovnica.naziv_poslovnice==""||poslovnica.OIB_poslovnice.Length!=11||poslovnica.adresa_poslovnice=="")
+            {
+                return false;
+            }
+            else
+                return true;
+        }
+    }
+}
