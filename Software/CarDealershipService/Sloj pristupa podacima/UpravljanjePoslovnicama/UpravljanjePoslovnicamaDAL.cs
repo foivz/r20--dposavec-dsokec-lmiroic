35d63a4da52b9ac8f7549c76c19f770b9484676b
Luka <lmiroic@foi.hr>
2020-06-19 16:44:06 +0200

    Izrađene funckije za kreiranje. brisanje i ažuriranje poslovnice u klasi UpravljanjePoslovnicamaDAL.

--- Software/CarDealershipService/Sloj pristupa podacima/UpravljanjePoslovnicama/UpravljanjePoslovnicamaDAL.cs
+++ Software/CarDealershipService/Sloj pristupa podacima/UpravljanjePoslovnicama/UpravljanjePoslovnicamaDAL.cs
@@ -17,5 +17,39 @@ namespace Sloj_pristupa_podacima.UpravljanjePoslovnicama
             }
             return SvePoslovnice;
         }
+        public static void KreirajPoslovnicu(Poslovnica poslovnica)
+        {
+            using (var db = new CarDealershipandServiceEntities())
+            {
+                db.Poslovnicas.Add(poslovnica);
+                db.SaveChanges();
+            }
+        }
+        public static void BrisanjePoslovnice(Poslovnica poslovnica)
+        {
+            using (var db = new CarDealershipandServiceEntities())
+            {
+                var selectedItem = db.Poslovnicas.Where(p => p.id_poslovnica == poslovnica.id_poslovnica).FirstOrDefault();
+                db.Poslovnicas.Remove(selectedItem);
+                db.SaveChanges();
+            }
+        }
+
+        public static void AzurirajPoslovnicu(Poslovnica poslovnica)
+        {
+            int id_poslovnice = poslovnica.id_poslovnica;
+            using (var db = new CarDealershipandServiceEntities())
+            {
+                Poslovnica novaPoslovnica = (from p in db.Poslovnicas
+                                      where p.id_poslovnica == id_poslovnice
+                                      select p).SingleOrDefault();
+                db.Poslovnicas.Attach(novaPoslovnica);
+                novaPoslovnica.id_poslovnica = poslovnica.id_poslovnica;
+                novaPoslovnica.naziv_poslovnice = poslovnica.naziv_poslovnice;
+                novaPoslovnica.adresa_poslovnice = poslovnica.adresa_poslovnice;
+                novaPoslovnica.OIB_poslovnice = poslovnica.OIB_poslovnice;
+                db.SaveChanges();
+            }
+        }
     }
 }
