fc6d3bf0be3c97dad5c86c66aed90c72c63a3dea
Luka <lmiroic@foi.hr>
2020-06-19 12:04:34 +0200

    Kreirane funkcije DohvatiSveTipoveKorisnika, KreiranjeKorisnika, BrisanjeKorisnika, AzuriranjeKorisnika unutar DAL klase UpravljanjeKorisnicima.

--- Software/CarDealershipService/Sloj pristupa podacima/UpravljanjeKorisnicima/UpravljanjeKorisnicima.cs
+++ Software/CarDealershipService/Sloj pristupa podacima/UpravljanjeKorisnicima/UpravljanjeKorisnicima.cs
@@ -18,6 +18,15 @@ namespace Sloj_pristupa_podacima.UpravljanjeKorisnicima
             }
             return SviKorisnici;
         }
+        public static List<Tip_korisnika> DohvatiSveTipoveKorisnika()
+        {
+            List<Tip_korisnika> SviTipoviKorisnika = null;
+            using (var db = new CarDealershipandServiceEntities())
+            {
+                SviTipoviKorisnika = db.Tip_korisnika.ToList();
+            }
+            return SviTipoviKorisnika;
+        }
 
         public static string HashirajLozinku(string input)
         {
@@ -62,6 +71,45 @@ namespace Sloj_pristupa_podacima.UpravljanjeKorisnicima
             }*/
             pronadenKorisnik = DohvatiSveKorisnike().Find(k => k.korisnicko_ime == korisnickoIme && k.lozinka == HashirajLozinku(lozinka));
             return pronadenKorisnik;
+        }
+        public static void KreiranjeKorisnika(Korisnik korisnik)
+        {
+            using (var db = new CarDealershipandServiceEntities())
+            {
+                db.Korisniks.Add(korisnik);
+                db.SaveChanges();
+            }
+        }
+        public static void BrisanjeKorisnika(Korisnik korisnik)
+        {
+            using (var db = new CarDealershipandServiceEntities())
+            {
+                var selectedItem = db.Korisniks.Where(t => t.id_korisnik == korisnik.id_korisnik).FirstOrDefault();
+                db.Korisniks.Remove(selectedItem);
+                db.SaveChanges();
+            }
+        }
+        public static void AzurirajKorisnika(Korisnik korisnik)
+        {
+            int id_korisnik = korisnik.id_korisnik;
+            using (var db = new CarDealershipandServiceEntities())
+            {
+                Korisnik noviKorisnik = (from k in db.Korisniks
+                                      where k.id_korisnik == id_korisnik
+                                      select k).SingleOrDefault();
+                db.Korisniks.Attach(noviKorisnik);
+                noviKorisnik.id_korisnik = korisnik.id_korisnik;
+                noviKorisnik.ime_korisnika = korisnik.ime_korisnika;
+                noviKorisnik.prezime_korisnika = korisnik.prezime_korisnika;
+                noviKorisnik.adresa = korisnik.adresa;
+                noviKorisnik.email = korisnik.email;
+                noviKorisnik.IBAN = korisnik.IBAN;
+                noviKorisnik.korisnicko_ime = korisnik.korisnicko_ime;
+                noviKorisnik.lozinka = korisnik.lozinka;
+                noviKorisnik.tip_korisnika = korisnik.tip_korisnika;
+                noviKorisnik.poslovnica = korisnik.poslovnica;
+                db.SaveChanges();
+            }
         }
     }
 }
