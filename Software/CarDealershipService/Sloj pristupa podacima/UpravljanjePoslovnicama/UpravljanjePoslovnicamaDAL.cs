dcc20ee4dd479fa0dfd8690f14235ed54095d147
Luka <lmiroic@foi.hr>
2020-06-19 12:10:33 +0200

    Kreirana klasa UpravljanjePoslovnicamaDAL u kojoj je implementirano dohvaćanje svih poslovnica iz baze podataka.

---
+++ Software/CarDealershipService/Sloj pristupa podacima/UpravljanjePoslovnicama/UpravljanjePoslovnicamaDAL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sloj_pristupa_podacima.UpravljanjePoslovnicama
+{
+    public class UpravljanjePoslovnicamaDAL
+    {
+        public static List<Poslovnica> DohvatiSvePoslovnice()
+        {
+            List<Poslovnica> SvePoslovnice = null;
+            using (var db = new CarDealershipandServiceEntities())
+            {
+                SvePoslovnice = db.Poslovnicas.ToList();
+            }
+            return SvePoslovnice;
+        }
+    }
+}
