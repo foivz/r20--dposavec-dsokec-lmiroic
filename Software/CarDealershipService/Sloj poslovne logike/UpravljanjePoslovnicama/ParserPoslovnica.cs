9caec32d9866c8307e5a99df4c749b3c089f0838
Luka <lmiroic@foi.hr>
2020-06-19 12:08:52 +0200

    Kreirana klasa ParserPoslovnica koja prosljeđuje listu svih poslovnica.

---
+++ Software/CarDealershipService/Sloj poslovne logike/UpravljanjePoslovnicama/ParserPoslovnica.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sloj_poslovne_logike.UpravljanjePoslovnicama
+{
+    public class ParserPoslovnica
+    {
+        public static List<Poslovnica> ParsirajPoslovnicu()
+        {
+            List<Poslovnica> poslovnicas = new List<Poslovnica>();
+            foreach (var item in Sloj_pristupa_podacima.UpravljanjePoslovnicama.UpravljanjePoslovnicamaDAL.DohvatiSvePoslovnice())
+            {
+                Poslovnica p = new Poslovnica();
+                p.id_poslovnica = item.id_poslovnica;
+                p.adresa_poslovnice = item.adresa_poslovnice;
+                p.naziv_poslovnice = item.naziv_poslovnice;
+                p.OIB_poslovnice = item.OIB_poslovnice;
+                poslovnicas.Add(p);
+            }
+            return poslovnicas;
+        }
+    }
+}
