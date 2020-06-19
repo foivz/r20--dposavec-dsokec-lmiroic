2e3f5a6f038a13e023f7610ab41245349d6ae325
Luka <lmiroic@foi.hr>
2020-06-19 21:52:20 +0200

    Implementirana provjera korisničkog unosa u formi KreirajNarudzbu.

--- Software/CarDealershipService/Prezentacijski sloj/FormKreirajNarudzbu.cs
+++ Software/CarDealershipService/Prezentacijski sloj/FormKreirajNarudzbu.cs
@@ -37,6 +37,7 @@ namespace Prezentacijski_sloj
         private void FormKreirajNarudzbu_FormClosed(object sender, FormClosedEventArgs e)
         {
             _Instance = null;
+            proslijedeniDokument = null;
         }
 
         private void FormKreirajNarudzbu_Load(object sender, EventArgs e)
@@ -52,37 +53,71 @@ namespace Prezentacijski_sloj
                 uiInputUkupniSaldo.Text = proslijedeniDokument.ukupni_saldo.ToString();
                 cbInputKorisnikKreirajNarudzbu.SelectedIndex = listaKorisnika.IndexOf(listaKorisnika.Find(x => x.id_korisnik == proslijedeniDokument.korisnik));
                 cbInputZaposlenikKreirajNarudzbu.SelectedIndex = listaZaposlenika.IndexOf(listaZaposlenika.Find(x => x.id_korisnik == proslijedeniDokument.zaposlenik));
+                uiActionSpremiNarudzbu.Enabled = false;
+                uiActionSpremiNarudzbu.Hide();
+            }
+            else
+            {
+                uiActionAzurirajNarudzbu.Enabled=false;
+                uiActionAzurirajNarudzbu.Hide();
             }
         }
 
         private void uiActionSpremiNarudzbu_Click(object sender, EventArgs e)
         {
             Sloj_pristupa_podacima.Dokument narudzba = new Sloj_pristupa_podacima.Dokument();
-            narudzba.datum_izdavanja = DateTime.Parse(dateTimeInputDatumIzdavanjaKreirajNarudzbu.Text.ToString());
-            narudzba.opis_dokumenta = uiInputOpisDokumentaKn.Text;
-            narudzba.ukupni_saldo = float.Parse(uiInputUkupniSaldo.Text);
-            narudzba.tip_dokumenta = 2;
-            narudzba.korisnik = (cbInputKorisnikKreirajNarudzbu.SelectedItem as Sloj_pristupa_podacima.Korisnik).id_korisnik;
-            narudzba.zaposlenik = (cbInputZaposlenikKreirajNarudzbu.SelectedItem as Sloj_pristupa_podacima.Korisnik).id_korisnik;
+            try
+            {
+                narudzba.datum_izdavanja = DateTime.Parse(dateTimeInputDatumIzdavanjaKreirajNarudzbu.Text.ToString());
+                narudzba.opis_dokumenta = uiInputOpisDokumentaKn.Text;
+                narudzba.ukupni_saldo = float.Parse(uiInputUkupniSaldo.Text);
+                narudzba.tip_dokumenta = 2;
+                narudzba.korisnik = (cbInputKorisnikKreirajNarudzbu.SelectedItem as Sloj_pristupa_podacima.Korisnik).id_korisnik;
+                narudzba.zaposlenik = (cbInputZaposlenikKreirajNarudzbu.SelectedItem as Sloj_pristupa_podacima.Korisnik).id_korisnik;
+                if (UpravljanjeRezervacijamaBLL.ProvjeraUnosaRezervacije(narudzba)==true)
+                {
+                    Sloj_pristupa_podacima.UpravljanjeNarudzbama.UpravljanjeNarudzbamaDAL.KreirajNarudzbu(narudzba);
+                    FormUpravljanjeNarudzbama.OsvjeziPrikaz();
+                }
+                else
+                {
+                    MessageBox.Show("Niste unijeli odgovarajuće parametre! Za pomoć pritisnite F1.");
+                }
+            }
+            catch (Exception)
+            {
 
-            Sloj_pristupa_podacima.UpravljanjeNarudzbama.UpravljanjeNarudzbamaDAL.KreirajNarudzbu(narudzba);
-            FormUpravljanjeNarudzbama.OsvjeziPrikaz();
+                MessageBox.Show("Morate unijeti sve parametre!");
+            }
         }
 
         private void uiActionAzurirajNarudzbu_Click(object sender, EventArgs e)
         {
             Sloj_pristupa_podacima.Dokument narudzba = new Sloj_pristupa_podacima.Dokument();
-            narudzba.id_dokument = proslijedeniDokument.id_dokument;
-            narudzba.datum_izdavanja = DateTime.Parse(dateTimeInputDatumIzdavanjaKreirajNarudzbu.Text.ToString());
-            narudzba.opis_dokumenta = uiInputOpisDokumentaKn.Text;
-            narudzba.ukupni_saldo = float.Parse(uiInputUkupniSaldo.Text);
-            narudzba.tip_dokumenta = 2;
-            narudzba.korisnik = (cbInputKorisnikKreirajNarudzbu.SelectedItem as Sloj_pristupa_podacima.Korisnik).id_korisnik;
-            narudzba.zaposlenik = (cbInputZaposlenikKreirajNarudzbu.SelectedItem as Sloj_pristupa_podacima.Korisnik).id_korisnik;
+            try
+            {
+                narudzba.id_dokument = proslijedeniDokument.id_dokument;
+                narudzba.datum_izdavanja = DateTime.Parse(dateTimeInputDatumIzdavanjaKreirajNarudzbu.Text.ToString());
+                narudzba.opis_dokumenta = uiInputOpisDokumentaKn.Text;
+                narudzba.ukupni_saldo = float.Parse(uiInputUkupniSaldo.Text);
+                narudzba.tip_dokumenta = 2;
+                narudzba.korisnik = (cbInputKorisnikKreirajNarudzbu.SelectedItem as Sloj_pristupa_podacima.Korisnik).id_korisnik;
+                narudzba.zaposlenik = (cbInputZaposlenikKreirajNarudzbu.SelectedItem as Sloj_pristupa_podacima.Korisnik).id_korisnik;
+                if (UpravljanjeRezervacijamaBLL.ProvjeraUnosaRezervacije(narudzba) == true)
+                {
+                    Sloj_pristupa_podacima.UpravljanjeNarudzbama.UpravljanjeNarudzbamaDAL.AzurirajNarudzbu(narudzba);
+                    FormUpravljanjeNarudzbama.OsvjeziPrikaz();
+                }
+                else
+                {
+                    MessageBox.Show("Niste unijeli odgovarajuće parametre! Za pomoć pritisnite F1.");
+                }
+            }
+            catch (Exception)
+            {
 
-            Sloj_pristupa_podacima.UpravljanjeNarudzbama.UpravljanjeNarudzbamaDAL.AzurirajNarudzbu(narudzba);
-            FormUpravljanjeNarudzbama.OsvjeziPrikaz();
-            proslijedeniDokument = null;
+                MessageBox.Show("Morate unijeti sve parametre!");
+            }
         }
     }
 }
