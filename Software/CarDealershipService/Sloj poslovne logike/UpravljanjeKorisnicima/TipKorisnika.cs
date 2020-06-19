c5846f0a19f75064ad44cceed126d698059e54f3
Luka <lmiroic@foi.hr>
2020-06-19 12:07:47 +0200

    Kreirana klasa TipKorisnika koja nasljeđuje klasu Tip_korisnika te sadrži override ToStrin() funkciju.

---
+++ Software/CarDealershipService/Sloj poslovne logike/UpravljanjeKorisnicima/TipKorisnika.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sloj_poslovne_logike.UpravljanjeKorisnicima
+{
+    public class TipKorisnika:Sloj_pristupa_podacima.Tip_korisnika
+    {
+        public override string ToString()
+        {
+            return naziv_uloge;
+        }
+    }
+}
