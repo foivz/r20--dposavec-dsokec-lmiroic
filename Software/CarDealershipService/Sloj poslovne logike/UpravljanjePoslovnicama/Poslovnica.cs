5d496aafdea969cbe8195a0f52193433742f0568
Luka <lmiroic@foi.hr>
2020-06-19 12:09:38 +0200

    Kreirana klasa Poslovnica koja nasljeđuje klasu Poslovnica te sadrži funckiju override ToString();

---
+++ Software/CarDealershipService/Sloj poslovne logike/UpravljanjePoslovnicama/Poslovnica.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sloj_poslovne_logike.UpravljanjePoslovnicama
+{
+    public class Poslovnica:Sloj_pristupa_podacima.Poslovnica
+    {
+        public override string ToString()
+        {
+            return naziv_poslovnice;
+        }
+    }
+}
