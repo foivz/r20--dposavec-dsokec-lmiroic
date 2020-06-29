dbc404795503935139b774576e6ce4762dc20108
Luka <lmiroic@foi.hr>
2020-06-29 22:50:58 +0200

    Kreirana klasa UpravljanjeUslugamaDAL u kojoj su kreirane funkcije za kreiranje usluga te vraćanje zadnjeg unosa.

---
+++ Software/CarDealershipService/Sloj pristupa podacima/Upravljanje uslugama/UpravljanjeUslugamaDAL.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sloj_pristupa_podacima.Upravljanje_uslugama
+{
+    public class UpravljanjeUslugamaDAL
+    {
+        public static void KreiranjeUsluge(Usluga usluga)
+        {
+            Boolean unos = false;
+            using (var db = new CarDealershipandServiceEntities())
+            {
+                foreach (var item in db.Uslugas)
+                {
+                    if (usluga.naziv_usluge.ToLower().Equals(item.naziv_usluge.ToLower()))
+                    {
+                        unos = true;
+                    }
+                    else
+                    {                        
+                        unos = false;
+                    }                        
+                }
+                if (!unos)
+                {
+                    db.Uslugas.Add(usluga);
+                }
+                db.SaveChanges();
+            }
+        }
+        public static Usluga VratiZadnjiUnos(string naziv)
+        {
+           
+            using (var db = new CarDealershipandServiceEntities())
+            {
+                foreach (var item in db.Uslugas)
+                {
+                    if (naziv.ToLower().Equals(item.naziv_usluge.ToLower()))
+                    {
+                        return item as Usluga;
+                    }
+                }
+                return null;
+                
+            }
+        }
+    }
+}
