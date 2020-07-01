66113290a920b57b298e5dee76970b6d1e7a542a
Dominik <dposavec@foi.hr>
2020-07-01 12:24:33 +0200

    Implementirane klase UpravljanjeDnevnikomRada, DnevnikLog i enumeracija RadnjaDnevnika sa operacijama vezane pri radu aplikacije.

---
+++ Software/CarDealershipService/Sloj poslovne logike/UpravljanjeDnevnikom/RadnjaDnevnika.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sloj_poslovne_logike.UpravljanjeDnevnikom
+{
+    public enum RadnjaDnevnika
+    {
+        PRIJAVA_U_SUSTAV = 1,
+        ODJAVA_IZ_SUSTAVA = 2,
+        KREIRANA_POSLOVNICA = 3,
+        KREIRAN_KORISNIK = 4,
+        KREIRANA_REZERVACIJA = 5,
+        KREIRANO_SKLADISTE = 1002,
+        KREIRANA_NARUDZBA = 1003,
+        KREIRANJE_VOZILA = 1004,
+        KREIRANJE_REZERVNOG_DIJELA = 1005,
+        BRISANJE_ARTIKLA = 1006,
+        BRISANJE_REZERVACIJE = 1007,
+        BRISANJE_NARUDZBE = 1008,
+        BRISANJE_KORISNIKA = 1009,
+        BRISANJE_POSLOVNICE = 1010,
+        PRODAN_ARTIKL = 1011
+    }
+}
