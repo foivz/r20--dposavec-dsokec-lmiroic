3f1f49f644618bbb7b26a1b014a33762cfe062b8
Luka <lmiroic@foi.hr>
2020-07-15 13:10:10 +0200

    Kreirana forma predviđanje u kojoj je realiziran algoritam predviđanja naručivanja artikala.

---
+++ Software/CarDealershipService/Prezentacijski sloj/FormPredvidanje.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Prezentacijski_sloj
+{
+    partial class FormPredvidanje
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbInputDobavljaci = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbInputDobavljaci
+            // 
+            this.cbInputDobavljaci.FormattingEnabled = true;
+            this.cbInputDobavljaci.Location = new System.Drawing.Point(646, 214);
+            this.cbInputDobavljaci.Name = "cbInputDobavljaci";
+            this.cbInputDobavljaci.Size = new System.Drawing.Size(142, 21);
+            this.cbInputDobavljaci.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.LightGray;
+            this.label1.Location = new System.Drawing.Point(643, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Odabir dobavljaca";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.LightGray;
+            this.label2.Location = new System.Drawing.Point(37, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Artikli";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.LightGray;
+            this.label3.Location = new System.Drawing.Point(251, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Datum narucivanja";
+            // 
+            // FormPredvidanje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbInputDobavljaci);
+            this.Name = "FormPredvidanje";
+            this.Text = "FormPredvidanje";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormPredvidanje_FormClosed);
+            this.Load += new System.EventHandler(this.FormPredvidanje_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbInputDobavljaci;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Software/CarDealershipService/Prezentacijski sloj/FormPredvidanje.cs
@@ -0,0 +1,53 @@
+﻿using Sloj_poslovne_logike;
+using Sloj_pristupa_podacima;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prezentacijski_sloj
+{
+    public partial class FormPredvidanje : Form
+    {
+        private static FormPredvidanje _Instance;
+        public static FormPredvidanje Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new FormPredvidanje();
+                }
+                return _Instance;
+            }
+        }
+        public FormPredvidanje()
+        {
+            InitializeComponent();
+        }
+        private void FormPredvidanje_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(45, 45, 48);
+            List<Sloj_poslovne_logike.UpravljanjeRezervacijama.Korisnik> dobavljaci = Sloj_poslovne_logike.UpravljanjeRezervacijama.ParserKorisnik.ParsirajDobavljača();
+            cbInputDobavljaci.DataSource = null;
+            cbInputDobavljaci.DataSource = dobavljaci;
+            List<Artikli_na_skladistu> artikli_Na_Skladistu = Sloj_pristupa_podacima.UpravljanjeSkladistem.UpravljanjeSkladistemDAL.DohvatiSveProdaneArtikleNaSkladistu(Sesija.PrijavljenKorisnik);
+            foreach (var item in artikli_Na_Skladistu)
+            {
+                string datum= Prediction.PredvidiVrijemeNarudzbe(item);
+                DinamicController.DodajRed(this,item,datum,cbInputDobavljaci.SelectedItem as Sloj_poslovne_logike.UpravljanjeRezervacijama.Korisnik);
+            }
+        }
+
+        private void FormPredvidanje_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _Instance = null;
+            this.Controls.Clear();
+        }
+    }
+}
